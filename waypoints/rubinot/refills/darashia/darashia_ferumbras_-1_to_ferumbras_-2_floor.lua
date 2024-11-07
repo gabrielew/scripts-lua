006daaa09a66a600f6f64850b4ad0beb64d08e5f
Gabriel <gabrielalcantaratp@gmail.com>
2024-11-07 08:05:34 -0300

    feat: add refill modules

---
+++ waypoints/rubinot/refills/darashia/darashia_ferumbras_-1_to_ferumbras_-2_floor.lua
@@ -0,0 +1,12 @@
+COORDS = {
+  { q = 33314,      c = 32335, b = 13,    type = 'NODE', wait = 'SINGLE_STEP_DELAY', },
+  { q = 33308,      c = 32340, b = 13,    type = 'NODE', wait = 'SINGLE_STEP_DELAY', },
+  { q = 33302,      c = 32343, b = 13,    type = 'NODE', wait = 'SINGLE_STEP_DELAY', },
+  { q = 33298,      c = 32349, b = 13,    type = 'NODE', wait = 'SINGLE_STEP_DELAY', },
+  { q = 33303,      c = 32355, b = 13,    type = 'NODE', wait = 'SINGLE_STEP_DELAY', },
+  { q = 33306,      c = 32361, b = 13,    type = 'NODE', wait = 'SINGLE_STEP_DELAY', },
+  { q = 33309,      c = 32367, b = 13,    type = 'NODE', wait = 'SINGLE_STEP_DELAY', },
+  { q = 33309,      c = 32367, b = 13,    type = 'NODE', wait = 'SINGLE_STEP_DELAY', },
+  { q = 33309,      c = 32367, b = 13,    type = 'NODE', wait = 'SINGLE_STEP_DELAY', },
+  { findedId = 469, q = 33309, c = 32369, b = 13,        type = 'ENTER_HOLE',        wait = 'SINGLE_STEP_DELAY', },
+}
